f5e6ac07467abf2552302b3e307c6399bd63dd30
Madeline <madeline@littlstar.com>
2017-08-29 17:53:00 -0400

    feat(Hivemind): Add ZIP + S3 files for publish()
    This commit builds out the ability to pass complete ZIP files (useful
    for packaging folders) and S3 buckets for publishing functions to Lambda

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const Lambda = require('aws-sdk/clients/lambda')
 const EventEmitter = require('events')
 const through2 = require('through2')
+const path = require('path')
 const zip = require('yazl')
+const fs = require('fs')
 
 /**
  * Create distributed jobs using AWS Lambda functions
@@ -42,72 +44,80 @@ module.exports = class Hivemind extends EventEmitter {
    */
 
   publish(params, awsParams) {
-    if (!params.files || params.files.length === 0) {
-      throw new Error('You must specify files to publish a function')
+
+    const publishFunc = (code) => {
+      // Merge parameters and AWS parameters
+      // This is so add'l that only AWS cares about can be passed
+      const mergedParams = Object.assign({
+        Code: {
+          ZipFile: code
+        },
+        FunctionName: this.func.name,
+        Handler: this.func.handler || 'index.handler',
+        Role: this.func.role,
+        Runtime: this.func.runtime || 'nodejs6.10'
+      }, awsParams || {})
+
+      // Check if function already exists!
+      // This determines whether we will create or update the function
+      this.lambda.getFunction({
+        FunctionName: mergedParams.FunctionName
+      }, (err, res) => {
+        const deployCallback = (error) => {
+          if (err) {
+            return this.emit('error', error)
+          }
+
+          return this.emit('create')
+        }
+
+        if (err) {
+          if (err.statusCode === 404) {
+            this.lambda.createFunction(mergedParams, deployCallback)
+          } else {
+            this.emit('error', err)
+          }
+        }
+
+        this.lambda.updateFunctionCode({
+          FunctionName: res.Configuration.FunctionArn,
+          ZipFile: mergedParams.Code.ZipFile
+        }, deployCallback)
+      })
     }
 
-    // Build ZIP file stream from passed functions
-    const file = new zip.ZipFile()
+    if (params.zipFile) {
+      publishFunc(fs.readFileSync(path.resolve(params.zipFile)))
+    } else if (params.files) {
+      // Build ZIP file stream from passed functions
+      const file = new zip.ZipFile()
 
-    // Add files to zip file
-    params.files.forEach(filePath => file.addFile(filePath, filePath))
+      // Add files to zip file
+      params.files.forEach(filePath => file.addFile(filePath, filePath))
 
-    // Signal that there are no more files
-    file.end()
+      // Signal that there are no more files
+      file.end()
 
-    // Array to contain buffers
-    let buff = Buffer.alloc(0)
+      // Array to contain buffers
+      let buff = Buffer.alloc(0)
 
-    // We have to pipe the stream through 'through' so we can extract
-    // the buffers that are contained in the stream
-    file.outputStream
-      .pipe(through2((chunk, _, callback) => {
+      // We have to pipe the stream through 'through' so we can extract
+      // the buffers that are contained in the stream
+      file.outputStream
+        .pipe(through2((chunk, _, callback) => {
 
-        // Push buffers to buffer
-        buff = Buffer.concat([buff, chunk])
-        return callback()
-      }))
-      .on('finish', () => {
-
-        // Merge parameters and AWS parameters
-        // This is so add'l that only AWS cares about can be passed
-        const mergedParams = Object.assign({
-          Code: {
-            ZipFile: buff
-          },
-          FunctionName: this.func.name,
-          Handler: this.func.handler || 'index.handler',
-          Role: this.func.role,
-          Runtime: this.func.runtime || 'nodejs6.10'
-        }, awsParams)
-
-        // Check if function already exists!
-        // This determines whether we will create or update the function
-        this.lambda.getFunction({
-          FunctionName: mergedParams.FunctionName
-        }, (err) => {
-          const deployCallback = (error) => {
-            if (err) {
-              return this.emit('error', error)
-            }
-
-            return this.emit('create')
-          }
-
-          if (err) {
-            if (err.statusCode === 404) {
-              this.lambda.createFunction(mergedParams, deployCallback)
-            } else {
-              this.emit('error', err)
-            }
-          }
-
-          this.lambda.updateFunctionCode({
-            FunctionName: mergedParams.FunctionName,
-            ZipFile: mergedParams.Code.ZipFile
-          }, deployCallback)
+          // Push buffers to buffer
+          buff = Buffer.concat([buff, chunk])
+          return callback()
+        }))
+        .on('finish', () => {
+          publishFunc(buff)
         })
-      })
+    } else if (awsParams.Code.S3Key) {
+      publishFunc()
+    } else {
+      throw new Error('You have to specify a code location to publish a function')
+    }
   }
 
   /**
